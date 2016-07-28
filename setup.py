1a3e0fb1da3d6c3b5539849aa80fc727b6a61bc4
malachite_sprite <m4rch3r@gmail.com>
2016-07-28 15:01:38 -0700

    fixed coverage reporting and requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup
 
+# deploy steps:
+# $ virtualenv -p python3 --no-site-packages ./
+# $ pip install -r requirements.txt
+# $ python setup.py install
+
+# test steps:
+# $ coverage run setup.py test
+# $ coverage report
+
 setup(name='cerberus',
     packages=['cerberus'],
     license='MIT',
